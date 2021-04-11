c7c9089639936f8c0dbcf9bb7493460490464208
jose ferreira <zejulio3@hotmail.com>
2021-04-11 18:15:54 +0200

    changed footer, navbar, used bootstraps, worked on responsiveness

--- src/contents/Footer.js
+++ src/contents/Footer.js
@@ -1,13 +1,18 @@
 import React from "react"
 import "./Footer.css"
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 function footer() {
     return (
-        <div className="Footer">
+        <container>
+          <row>
             <a href="https://www.linkedin.com/in/jose-julio-ferreira/">LinkedIN</a>
             <a href="https://github.com/josejulio28">GitHub</a>
             <a href="zejulio3@hotmail.com">Email</a>
-        </div>
+          </row>
+        </container>
     )
 }
 
--- src/contents/portfolio.js
+++ src/contents/portfolio.js
@@ -2,21 +2,24 @@ import React from "react"
 import "./portfolio.css"
 import pets from "./pets_img.png"
 import survivors from "./survivors_img.png"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 function portfolio() {
     return (
-        <div className="div-portfolio">
-          <a href="https://www.womensurvivors.net/"> </a>
-          <div className="card-portfolio" >
-            <h2>Survivors</h2>
+        <div className="row">
+          <div className="col-12 col-sm-6 col-md-3 card-portfolio" >
+            <h2><a href="https://www.womensurvivors.net/" target="_blank">Survivors</a></h2>
                 <img className="img-portfolio" src={survivors} alt="image_survivors"/>
                 <p></p>
             </div>
-            <div className="card-portfolio">
-                <h2><a href="https://book-a-pet.herokuapp.com/">Pets</a></h2>
+            <div className="col-12 col-sm-6 col-md-3 card-portfolio">
+                <h2><a href="https://book-a-pet.herokuapp.com/" target="_blank">Pets</a></h2>
                 <img className="img-portfolio" src={pets} alt="image_of_pet"/>
                 <p></p></div>
-            <div className="card-portfolio">
+            <div className="col-12 col-sm-6 col-md-3 card-portfolio ">
                 <h2> <a href="https">Mr Cocktail</a></h2>
                 <p></p></div>
         </div>
