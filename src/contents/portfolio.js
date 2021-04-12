57cf53295dbe8c8937a1611e4a2139ceb0cc917c
jose ferreira <zejulio3@hotmail.com>
2021-04-12 10:19:48 +0200

    change in the about me page, put a container. changed tech skills padding on h2

--- src/contents/portfolio.js
+++ src/contents/portfolio.js
@@ -9,9 +9,10 @@ import Col from 'react-bootstrap/Col'
 
 function portfolio() {
     return (
-        <div className="row">
-          <div className="col-12 col-sm-6 col-md-3 card-portfolio" >
-            <h2><a href="https://www.womensurvivors.net/" target="_blank">Survivors</a></h2>
+        <Container>
+          <Row>
+            <div className="col-12 col-sm-6 col-md-3 card-portfolio" >
+              <h2><a href="https://www.womensurvivors.net/" target="_blank">Survivors</a></h2>
                 <img className="img-portfolio" src={survivors} alt="image_survivors"/>
                 <p></p>
             </div>
@@ -20,9 +21,10 @@ function portfolio() {
                 <img className="img-portfolio" src={pets} alt="image_of_pet"/>
                 <p></p></div>
             <div className="col-12 col-sm-6 col-md-3 card-portfolio ">
-                <h2> <a href="https">Mr Cocktail</a></h2>
-                <p></p></div>
-        </div>
+              <h2> <a href="https">Mr Cocktail</a></h2>
+              <p></p></div>
+          </Row>
+        </Container>
     )
 }
 
